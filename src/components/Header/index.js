299e8effe88a31c5e4b215d573d3bb8b578f28dd
jamesdeanbarn <jamesdeanbarnett@gmail.com>
2020-10-28 11:09:43 +0000

    added css styling to searchicon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,7 +21,7 @@ export default class Header extends Component {
                 </div>
                 <div className="header__input">
                     <input type="text"/>
-                    <SearchIcon />
+                    <SearchIcon className="header__inputButton"/>
                 </div>
                     <div className="header__icons">
                     <VideoCallIcon />
